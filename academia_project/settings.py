f00722069b2b1f9465b23e9de27fe63d08e78e29
lucasmartinoviedo771-design <lucasmartinoviedo771@gmail.com>
2025-08-31 01:37:08 -0300

    Update settings.py

--- academia_project/settings.py
+++ academia_project/settings.py
@@ -182,9 +182,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # =========== i18n ===========
 LANGUAGE_CODE = "es-ar"
-TIME_ZONE = "America/Argentina/Buenos_ Aires"
+
+# Usa la TZ real por defecto, pero permite override por ENV (para CI)
+TIME_ZONE = os.getenv("TIME_ZONE", "America/Argentina/Buenos_Aires")
+
 USE_I18N = True
 USE_TZ = True
+
 
 # =========== Static & Media ===========
 STATIC_URL = "/static/"
